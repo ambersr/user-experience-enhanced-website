82c835b7fd0624e64f24dacd411ef1aa5c4900be
ambersr <amber.schalker@hva.nl>
2025-04-21 12:47:39 +0200

    Setup Express server.js met Liquid templating #2

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-console.log('Hier komt je server voor Sprint 10.')
+// Importeer packages
+import express from 'express'
+import { Liquid } from 'liquidjs';
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// Express app setup
+const app = express()
 
-console.log('Zet \'m op!')
+// Statische bestanden
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views')
+
+// Lokaal server port opzetten
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}`)
+})
