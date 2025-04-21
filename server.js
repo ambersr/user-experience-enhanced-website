284646b50f27a045c3cb534bfd27f95a8b047cc0
ambersr <amber.schalker@hva.nl>
2025-04-21 13:05:24 +0200

    Algemene API's opgezet #3

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// ----------------------------------------- Setup Express/Liquid -----------------------------------------
+
 // Importeer packages
 import express from 'express'
 import { Liquid } from 'liquidjs';
@@ -13,6 +15,17 @@ app.use(express.static('public'))
 const engine = new Liquid();
 app.engine('liquid', engine.express());
 app.set('views', './views')
+
+// ----------------------------------------- Algemene API's -----------------------------------------
+
+// Algemene API linkjes
+const messagesLink = "https://fdnd-agency.directus.app/items/avl_messages";
+const webinarsLink = "https://fdnd-agency.directus.app/items/avl_webinars";
+const categoryLink = "https://fdnd-agency.directus.app/items/avl_categories";
+const contouringsLink = "https://fdnd-agency.directus.app/items/avl_contourings";
+const webinarsField = "?fields=id,duration,title,slug,date,video,thumbnail,.*.*,speakers.*.*,categories.avl_categories_id.*,resources.*.*";
+const contouringsField = "?fields=title,image_scan,user_id.fullname,categories.avl_categories_id.*";
+const messagesFilter = "?filter[for][_eq]=Watchlist Amber"
 
 // Lokaal server port opzetten
 app.set('port', process.env.PORT || 8000)
